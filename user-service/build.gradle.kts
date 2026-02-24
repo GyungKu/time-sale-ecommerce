7b419f1835b36797e0593f38d4e2ac1272f22234
gk <iba3374@naver.com>
2026-02-25 00:48:54 +0900

    chore: User 서버 멀티 모듈 구조 수정

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -22,11 +22,20 @@ subprojects {
         mavenCentral()
     }
 
+    // Spring Boot 의존성 버전을 하위 모듈에 일괄 적용
+    dependencyManagement {
+        imports {
+            mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+        }
+    }
+
     dependencies {
         // 모든 모듈에서 공통으로 사용할 라이브러리 (Lombok 등)
         compileOnly("org.projectlombok:lombok")
         annotationProcessor("org.projectlombok:lombok")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testCompileOnly("org.projectlombok:lombok")
+        testAnnotationProcessor("org.projectlombok:lombok")
     }
 
     tasks.withType<Test> {
--- user-service/user-infrastructure/build.gradle.kts
+++ user-service/user-infrastructure/build.gradle.kts
@@ -1,5 +1,11 @@
+tasks.bootJar { enabled = false }
+tasks.jar { enabled = true }
+
 dependencies {
+    // 도메인(Repository 인터페이스 등)과 애플리케이션 계층 참조
     implementation(project(":user-domain"))
+    implementation(project(":user-application"))
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.postgresql:postgresql")
     runtimeOnly("com.h2database:h2")
