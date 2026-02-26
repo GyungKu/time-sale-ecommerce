fd040502f93b41a4e445b0b834d072dc7a5d8da5
gk <iba3374@naver.com>
2026-02-27 01:31:51 +0900

    chore: 각 api에서 infrastructure 의존 runtimeOnly로 변경

--- user-service/user-api/build.gradle.kts
+++ user-service/user-api/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     // Use Case 호출을 위해 application 모듈 참조
     implementation(project(":user-service:user-application"))
-    implementation(project(":user-service:user-infrastructure"))
+    runtimeOnly(project(":user-service:user-infrastructure"))
     implementation(project(":common:common-exception"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
