bc6f28e867e540cae6b57e5e1961d4fecfa38f84
gk <iba3374@naver.com>
2026-02-27 01:31:51 +0900

    chore: 각 api에서 infrastructure 의존 runtimeOnly로 변경

--- product-service/product-api/build.gradle.kts
+++ product-service/product-api/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     implementation(project(":product-service:product-application"))
     implementation(project(":product-service:product-domain"))
-    implementation(project(":product-service:product-infrastructure"))
+    runtimeOnly(project(":product-service:product-infrastructure"))
 
     implementation(project(":common:common-exception"))
     implementation("org.springframework.boot:spring-boot-starter-web")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,9 @@ include("product-service:product-api")
 include("product-service:product-application")
 include("product-service:product-domain")
 include("product-service:product-infrastructure")
+
+// Order 서비스 모듈들 추가
+include("order-service:order-api")
+include("order-service:order-application")
+include("order-service:order-domain")
+include("order-service:order-infrastructure")
