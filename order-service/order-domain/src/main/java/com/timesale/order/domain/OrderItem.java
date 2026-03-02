49e244477eafdf36c1828be9bfaa3496ec1fe831
gk <iba3374@naver.com>
2026-03-02 20:35:18 +0900

    fix: 주문 서버 실패 핸들러 트랜잭션 수정 및 OrderItem Entity 수정

--- order-service/order-domain/src/main/java/com/timesale/order/domain/OrderItem.java
+++ order-service/order-domain/src/main/java/com/timesale/order/domain/OrderItem.java
@@ -1,15 +1,10 @@
 package com.timesale.order.domain;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -26,11 +21,6 @@ public class OrderItem {
     private Long id;
 
     private Long orderId;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "orderId", nullable = false, insertable = false, updatable = false,
-    foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-    private Order order;
 
     @Column(nullable = false)
     private Long productId;
